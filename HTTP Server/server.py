0852b8221eead0a9bf3a9f550bbf661ed0213653
Evan Pfeifer <50773875+evanpfeifer@users.noreply.github.com>
2019-05-25 18:33:30 -0600

    Add files via upload

---
+++ HTTP Server/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving as port", PORT)
+    httpd.serve_forever()
